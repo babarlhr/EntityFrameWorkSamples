22cc631e5f8a53187ce07267e44c4f746a291427
ishfaq babar <ishfaq_ciit@yahoo.com>
2023-06-05 12:55:46 +0500

    Add Directory Data To store DataBase Context Classes.

--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -10,8 +10,8 @@ namespace EntityFrameWorkSample.Models
     {
         public int Id { get; set; }
         public int Quantity { get; set; }
-        public int ProductId { get; set; }  //Forign Key Relationship with Product  in Database---Navigation Property for class to access by .
-        public int OrderId { get; set; } //Forign Key Relationship with Order in database---Navigation Property for class to access by .
+        public int ProductId { get; set; }  //Forign Key Relationship with Product  in Database---Navigation Property 
+        public int OrderId { get; set; } //Forign Key Relationship with Order in database---Navigation Property 
 
         public Order Order { get; set; } = null!;
 
