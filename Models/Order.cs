c8cc1940005f3934121f84118669222d8e299ace
ishfaq babar <ishfaq_ciit@yahoo.com>
2023-06-05 12:48:02 +0500

    Added Forign Key Relationship

--- Models/Order.cs
+++ Models/Order.cs
@@ -8,5 +8,14 @@ namespace EntityFrameWorkSample.Models
 {
     public class Order
     {
+        public int Id { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public DateTime? OrderFulFilled { get; set; }       
+        public int CustomerID { get; set; }
+
+        public Customer Customer { get; set; } = null!;
+
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+
     }
 }
