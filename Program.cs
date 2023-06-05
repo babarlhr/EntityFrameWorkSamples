d91ae18eedd7c2555f7a36b4094cc3021b7023b1
ishfaq babar <ishfaq_ciit@yahoo.com>
2023-06-05 17:32:44 +0500

    Updated Prgram.cs to use Product Table with Shop DbContext

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace EntityFrameWorkSample
+﻿using EntityFrameWorkSample.Data;    //Added to use DatabseContext class
+using EntityFrameWorkSample.Models;   //Added to retrieve and write data to database tables  using classes.
+
+
+namespace EntityFrameWorkSample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            using ShopContext context = new ShopContext();   //Using to delete,add and get data from Shop database using its context class.
+                                                             //using statemet to properly dispose off context object when we finished using it. 
+
+            Product product = new Product()
+            {
+                Name = "Test Product",
+                LastName ="any",
+                Price =12
+            };
+
+
         }
     }
 }
