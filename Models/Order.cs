a8893342147b91ef6d43142b55bfd4dc9731cc27
ishfaq babar <ishfaq_ciit@yahoo.com>
2023-06-05 12:54:04 +0500

    Added Order Detials class with Forign Key Relationship and Navigation Property

--- Models/Order.cs
+++ Models/Order.cs
@@ -11,11 +11,13 @@ namespace EntityFrameWorkSample.Models
         public int Id { get; set; }
         public DateTime OrderPlaced { get; set; }
         public DateTime? OrderFulFilled { get; set; }       
-        public int CustomerID { get; set; }
+        public int CustomerID { get; set; }  //Represent Foriegn key Relationship with Customer Table .
+                                             //If we omit this property in class it will be automatically created by EntityFramewrok
+                                             //because it is also called a shadow property.
 
-        public Customer Customer { get; set; } = null!;
+        public Customer Customer { get; set; } = null!; //Navigation property --- Forign key Relationship .. One customer ID for order.
 
-        public ICollection<OrderDetail> OrderDetails { get; set; } = null!;
+        public ICollection<OrderDetail> OrderDetails { get; set; } = null!; //Navigation property
 
     }
 }
--- Models/OrderDetail.cs
+++ Models/OrderDetail.cs
@@ -8,5 +8,13 @@ namespace EntityFrameWorkSample.Models
 {
     public class OrderDetail
     {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public int ProductId { get; set; }  //Forign Key Relationship with Product  in Database---Navigation Property for class to access by .
+        public int OrderId { get; set; } //Forign Key Relationship with Order in database---Navigation Property for class to access by .
+
+        public Order Order { get; set; } = null!;
+
+        public Product Product { get; set; } = null!;
     }
 }
